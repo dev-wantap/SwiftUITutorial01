a5421946508f7b9432dd46e5bacd5edddf062489
Gwanwoo Kim <gwkgwk1125@naver.com>
2020-09-13 18:47:55 +0900

    Initial Commit

---
+++ SwiftUITutorial01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial01
+//
+//  Created by wantap on 2020/09/13.
+//  Copyright © 2020 wantap. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
